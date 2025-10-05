07174871a68fea3b9b784ec69ad7ee9595398682
Angel <vs0aa231+thec@gmail.com>
2025-10-05 14:56:05 -0400

    Update fbconfig.js

--- fbconfig.js
+++ fbconfig.js
@@ -1,1 +1,9 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA9eLg5CJXzakMGEUfykW25Vay-pvcf2gQ",
+  authDomain: "wikicloneangel.firebaseapp.com",
+  projectId: "wikicloneangel",
+  storageBucket: "wikicloneangel.firebasestorage.app",
+  messagingSenderId: "326980840327",
+  appId: "1:326980840327:web:7b8cf54747be1adc1f8122",
+  measurementId: "G-XQ7ZHJ4DLN"
+};
