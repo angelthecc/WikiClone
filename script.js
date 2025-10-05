43601e3ccda938d31bbe0ccc809857a0c0e71639
Angel <vs0aa231+thec@gmail.com>
2025-10-05 15:17:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { db } from "./firebase-config.js";
+import { app, db } from './firebase-config.js';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
