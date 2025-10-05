31df02de3015b9602d1d703c748100606682f06e
Angel <vs0aa231+thec@gmail.com>
2025-10-05 15:03:21 -0400

    Update fbconfig.js

--- fbconfig.js
+++ fbconfig.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyA9eLg5CJXzakMGEUfykW25Vay-pvcf2gQ",
   authDomain: "wikicloneangel.firebaseapp.com",
@@ -7,3 +10,6 @@ const firebaseConfig = {
   appId: "1:326980840327:web:7b8cf54747be1adc1f8122",
   measurementId: "G-XQ7ZHJ4DLN"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
